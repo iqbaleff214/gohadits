a2db998461aa3c5084b2f9e26bab62fabf64d996
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-04-23 09:00:29 +0800

    clean up root and error handler

---
+++ handler.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// GET /api/v1/
+func rootV1Handler(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).JSON(map[string]any{
+		"owner":   "M. Iqbal Effendi <iqbaleff214@gmail.com>",
+		"license": "MIT",
+		"source":  "https://github.com/iqbaleff214/gohadits",
+		"version": "1",
+		"endpoints": []map[string]string{
+			{
+				"path":        "/api/v1/hadith",
+				"method":      "GET",
+				"description": "Mengembalikan daftar kitab hadits yang tersedia.",
+			},
+			{
+				"path":        "/api/v1/hadith/{book}",
+				"method":      "GET",
+				"description": "Mengembalikan daftar hadits yang terdapat pada kitab yang diminta.",
+			},
+			{
+				"path":        "/api/v1/hadith/{book}/{number}",
+				"method":      "GET",
+				"description": "Mengembalikan hadith spesifik.",
+			},
+		},
+	})
+}
+
+// Error handler response
+func errorHandler(c *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+
+	var e *fiber.Error
+	if errors.As(err, &e) {
+		code = e.Code
+	}
+
+	err = c.Status(code).JSON(map[string]any{
+		"code":    code,
+		"message": e.Message,
+		"status":  "error",
+	})
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(map[string]any{
+			"code":    fiber.StatusInternalServerError,
+			"message": "Internal Server Error",
+			"status":  "error",
+		})
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"embed"
-	"errors"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -37,32 +36,7 @@ func setup() *fiber.App {
 
 	// api version 1
 	apiV1 := api.Group("/v1")
-
-	apiV1.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON(map[string]any{
-			"owner":   "M. Iqbal Effendi <iqbaleff214@gmail.com>",
-			"license": "MIT",
-			"source":  "https://github.com/iqbaleff214/gohadits",
-			"version": "1",
-			"endpoints": []map[string]string{
-				{
-					"path":        "/api/v1/hadith",
-					"method":      "GET",
-					"description": "Mengembalikan daftar kitab hadits yang tersedia.",
-				},
-				{
-					"path":        "/api/v1/hadith/{book}",
-					"method":      "GET",
-					"description": "Mengembalikan daftar hadits yang terdapat pada kitab yang diminta.",
-				},
-				{
-					"path":        "/api/v1/hadith/{book}/{number}",
-					"method":      "GET",
-					"description": "Mengembalikan hadith spesifik.",
-				},
-			},
-		})
-	})
+	apiV1.Get("/", rootV1Handler)
 
 	// hadith v1 domain
 	hadithRoute := apiV1.Group("/hadith")
@@ -74,29 +48,7 @@ func setup() *fiber.App {
 // route configuration
 func config() fiber.Config {
 	return fiber.Config{
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			code := fiber.StatusInternalServerError
-
-			var e *fiber.Error
-			if errors.As(err, &e) {
-				code = e.Code
-			}
-
-			err = c.Status(code).JSON(map[string]any{
-				"code":    code,
-				"message": e.Message,
-				"status":  "error",
-			})
-			if err != nil {
-				return c.Status(fiber.StatusInternalServerError).JSON(map[string]any{
-					"code":    fiber.StatusInternalServerError,
-					"message": "Internal Server Error",
-					"status":  "error",
-				})
-			}
-
-			return nil
-		},
-		AppName: "GoHadits",
+		AppName:      "GoHadits",
+		ErrorHandler: errorHandler,
 	}
 }
